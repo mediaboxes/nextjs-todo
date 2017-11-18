e5d45890b0e63ee12a0f6ff6490372a5d2d51678
Mediaboxes <mediaboxes@gmail.com>
2017-11-18 20:30:07 +0900

    Add next and mysql project

---
+++ nextjs/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default () => (
+  <div>
+    <Head>
+      <title>title</title>
+      <meta charSet='utf-8' />
+      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+    </Head>
+
+    <h1>This page</h1>
+  </div>
+)
