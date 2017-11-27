1768aea7b9230479f0ee401ecd8b4072a95cf18c
Mediaboxes <mediaboxes@gmail.com>
2017-11-28 03:13:50 +0900

    Fix api sort

--- nextjs/api/utils.js
+++ nextjs/api/utils.js
@@ -3,7 +3,7 @@ const connection = require('./mysqlConnection')
 function sqlPromiss(querySql) {
   return new Promise((resolve, reject) => {
     connection.query(querySql, (error, results) => {
-      console.log('sqlPromiss', querySql, error, results)
+      // console.log('sqlPromiss', querySql, error, results)
       if (error) {
         reject(new Error(`SQL実行に失敗しました : ${error.message}`))
         return
